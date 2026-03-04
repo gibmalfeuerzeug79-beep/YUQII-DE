baec70300bb399d8a685fbf94e7109b2f310fcd6
gibmalfeuerzeug79-beep <gibmalfeuerzeug79@gmail.com>
2026-03-04 13:09:11 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import os 
+
 from flask import Flask, jsonify
 
 app = Flask(__name__)
@@ -19,4 +21,7 @@ def set_config(details, state):
     config["state"] = state
     return "Updated"
 
-app.run(host="0.0.0.0", port=3000)
+app.run(
+    host="0.0.0.0",
+    port=int(os.environ.get("PORT", 3000))
+)
