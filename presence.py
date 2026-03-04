73d318a8212ef44157407c1d712b1a2fd05c3218
gibmalfeuerzeug79-beep <gibmalfeuerzeug79@gmail.com>
2026-03-04 12:57:53 +0100

    Create presence.py

---
+++ presence.py
@@ -0,0 +1,28 @@
+from pypresence import Presence
+import requests
+import time
+
+CLIENT_ID = "1478716804636606627"
+CONFIG_URL = "https://dein-railway-projekt.up.railway.app/config"
+
+RPC = Presence(CLIENT_ID)
+RPC.connect()
+
+print("Rich Presence gestartet")
+
+while True:
+    try:
+        data = requests.get(CONFIG_URL).json()
+
+        RPC.update(
+            details=data["details"],
+            state=data["state"],
+            large_image=data["large_image"],
+            large_text=data["large_text"],
+            start=time.time()
+        )
+
+    except Exception as e:
+        print("Fehler:", e)
+
+    time.sleep(30)
